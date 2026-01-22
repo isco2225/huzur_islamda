32dbf8b559e560b34fe49dcff884c858bfeac0e4
isco2225 <omran.haj20@gmail.com>
2026-01-22 13:54:10 +0300

    Feat: small animation added to dhikr detail screen.

--- lib/ui/dhikr/dhikr_detail/views/dhikr_detail_view.dart
+++ lib/ui/dhikr/dhikr_detail/views/dhikr_detail_view.dart
@@ -4,22 +4,30 @@ import 'package:go_router/go_router.dart';
 import '../../../../app/app.dart';
 import '../../../ui.dart';
 
-class DhikrDetailView extends StatelessWidget {
+class DhikrDetailView extends StatefulWidget {
   const DhikrDetailView({super.key, required this.viewModel});
 
   final DhikrDetailViewModel viewModel;
 
   @override
+  State<DhikrDetailView> createState() => _DhikrDetailViewState();
+}
+
+class _DhikrDetailViewState extends State<DhikrDetailView> {
+  // Callback to trigger animation - better than GlobalKey
+  VoidCallback? _triggerNameAnimation;
+
+  @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
       listenable: Listenable.merge([
-        viewModel.loadDhikr.running,
-        viewModel.deleteDhikr.running,
+        widget.viewModel.loadDhikr.running,
+        widget.viewModel.deleteDhikr.running,
       ]),
       builder: (context, child) {
         final isLoading =
-            viewModel.loadDhikr.running.value ||
-            viewModel.deleteDhikr.running.value;
+            widget.viewModel.loadDhikr.running.value ||
+            widget.viewModel.deleteDhikr.running.value;
         if (isLoading) {
           return BaseScaffold(
             appBar: AppBar(backgroundColor: AppColors.background, elevation: 0),
@@ -29,7 +37,7 @@ class DhikrDetailView extends StatelessWidget {
         }
 
         return ValueListenableBuilder(
-          valueListenable: viewModel.currentDhikr,
+          valueListenable: widget.viewModel.currentDhikr,
           builder: (context, dhikr, child) {
             if (dhikr == null) {
               return BaseScaffold(
@@ -75,14 +83,12 @@ class DhikrDetailView extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(bottom: 100.0),
                     child: Center(
-                      child: Text(
-                        dhikr.name,
-                        style: Theme.of(context).textTheme.headlineMedium
-                            ?.copyWith(
-                              fontWeight: FontWeight.w600,
-                              fontStyle: FontStyle.italic,
-                              color: AppColors.success,
-                            ),
+                      child: _AnimatedDhikrName(
+                        name: dhikr.name,
+                        onAnimationReady: (callback) {
+                          // Store the callback to trigger animation later
+                          _triggerNameAnimation = callback;
+                        },
                       ),
                     ),
                   ),
@@ -90,7 +96,7 @@ class DhikrDetailView extends StatelessWidget {
                   DhikrProgressIndicator(
                     currentCount: dhikr.currentCount,
                     targetCount: dhikr.targetCount,
-                    progress: viewModel.progress,
+                    progress: widget.viewModel.progress,
                   ),
                   SizedBox(height: context.spacingExtraLarge),
                   // Completion Status
@@ -101,10 +107,15 @@ class DhikrDetailView extends StatelessWidget {
                   // Counter Controls
                   DhikrCounterControls(
                     dhikr: dhikr,
-                    onIncrement: () => viewModel.incrementCount.execute(),
-                    onDecrement: () => viewModel.decrementCount.execute(),
-                    incrementRunning: viewModel.incrementCount.running,
-                    decrementRunning: viewModel.decrementCount.running,
+                    onIncrement: () {
+                      // Trigger animation when increment button is pressed
+                      _triggerNameAnimation?.call();
+                      widget.viewModel.incrementCount.execute();
+                    },
+                    onDecrement: () =>
+                        widget.viewModel.decrementCount.execute(),
+                    incrementRunning: widget.viewModel.incrementCount.running,
+                    decrementRunning: widget.viewModel.decrementCount.running,
                   ),
                 ],
               ),
@@ -129,7 +140,7 @@ class DhikrDetailView extends StatelessWidget {
           TextButton(
             onPressed: () {
               Navigator.pop(context);
-              viewModel.deleteDhikr.execute();
+              widget.viewModel.deleteDhikr.execute();
             },
             style: TextButton.styleFrom(foregroundColor: AppColors.error),
             child: const Text('Sil'),
@@ -153,12 +164,82 @@ class DhikrDetailView extends StatelessWidget {
           TextButton(
             onPressed: () {
               Navigator.pop(context);
-              viewModel.resetCount.execute();
+              widget.viewModel.resetCount.execute();
             },
             child: const Text('Sıfırla'),
           ),
         ],
       ),
+    );
+  }
+}
+
+/// Animated widget for displaying dhikr name with pop animation on increment
+class _AnimatedDhikrName extends StatefulWidget {
+  const _AnimatedDhikrName({
+    required this.name,
+    required this.onAnimationReady,
+  });
+
+  final String name;
+  final ValueChanged<VoidCallback> onAnimationReady;
+
+  @override
+  State<_AnimatedDhikrName> createState() => _AnimatedDhikrNameState();
+}
+
+class _AnimatedDhikrNameState extends State<_AnimatedDhikrName>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    // Color animation: from success to a brighter color and back
+    _colorAnimation = ColorTween(
+      begin: AppColors.primary.withValues(alpha: 0.8),
+      end: AppColors.primary,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    // Register the animation callback with parent
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      widget.onAnimationReady(triggerAnimation);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  /// Trigger animation when increment button is pressed
+  void triggerAnimation() {
+    _controller.forward(from: 0.0).then((_) {
+      _controller.reverse();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Text(
+          widget.name,
+          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+            fontWeight: FontWeight.w600,
+            fontStyle: FontStyle.italic,
+            color: _colorAnimation.value ?? AppColors.success,
+          ),
+        );
+      },
     );
   }
 }
