17318bb6fb11f4e0ab1e33d6a8167d6bdbb12a51
isco2225 <omran.haj20@gmail.com>
2026-02-03 14:29:13 +0300

    Refactor: Increase fetch limit for posts from 1 to 3.

--- lib/data/services/firestore_post_service.dart
+++ lib/data/services/firestore_post_service.dart
@@ -5,7 +5,7 @@ import '../../app/app.dart';
 class FirestorePostService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   static const String _collectionName = 'posts';
-  static const int _fetchLimit = 1;
+  static const int _fetchLimit = 3;
   // Todo(omran): Fetch posts that are not reported, active and not favorited by the current user.
   Future<Result<QuerySnapshot<Map<String, dynamic>>>> fetchPosts({
     DocumentSnapshot? lastFetchedPost,
