c3d225805605d1aa3be3d074606f49d84c9ac612
isco2225 <omran.haj20@gmail.com>
2026-02-14 15:01:00 +0300

    Refactor: Update AdMob ad unit IDs for production environment.

--- lib/app/core/constants/app_ad_ids.dart
+++ lib/app/core/constants/app_ad_ids.dart
@@ -1,30 +1,27 @@
 class AppAdIds {
   AppAdIds._();
 
-  /// AdMob Application ID (zaten AndroidManifest.xml ve Info.plist'te mevcut)
-  static const String applicationId = 'ca-app-pub-4675450525864095~8590581972';
-
   /// Banner Ad Unit ID - Android
   /// Test ID: ca-app-pub-3940256099942544/6300978111
   /// Production ID'yi AdMob konsolundan alın ve buraya ekleyin
   static const String bannerAdUnitIdAndroid =
-      'ca-app-pub-3940256099942544/6300978111';
+      'ca-app-pub-4675450525864095/2192694582';
 
   /// Banner Ad Unit ID - iOS
   /// Test ID: ca-app-pub-3940256099942544/2934735716
   /// Production ID'yi AdMob konsolundan alın ve buraya ekleyin
   static const String bannerAdUnitIdIOS =
-      'ca-app-pub-3940256099942544/2934735716';
+      'ca-app-pub-4675450525864095/9310458623';
 
   /// Interstitial Ad Unit ID - Android
   /// Test ID: ca-app-pub-3940256099942544/1033173712
   /// Production ID'yi AdMob konsolundan alın ve buraya ekleyin
   static const String interstitialAdUnitIdAndroid =
-      'ca-app-pub-3940256099942544/1033173712';
+      'ca-app-pub-4675450525864095/4507866733';
 
   /// Interstitial Ad Unit ID - iOS
   /// Test ID: ca-app-pub-3940256099942544/4411468910
   /// Production ID'yi AdMob konsolundan alın ve buraya ekleyin
   static const String interstitialAdUnitIdIOS =
-      'ca-app-pub-3940256099942544/4411468910';
+      'ca-app-pub-4675450525864095/3431445981';
 }
